0fdadd0cc3ccda2b46015faad63b35c849b1ea8f
DeGnoss <konrad.fellmann@gmail.com>
2016-12-13 22:09:52 +0100

    *updated database

--- app/src/main/java/com/pentapus/pentapusdmh/DbClasses/DataBaseHandler.java
+++ app/src/main/java/com/pentapus/pentapusdmh/DbClasses/DataBaseHandler.java
@@ -744,7 +744,6 @@ public class DataBaseHandler extends SQLiteAssetHelper {
     public DataBaseHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
-    
 
     /*@Override
     public void onCreate(SQLiteDatabase db) {
